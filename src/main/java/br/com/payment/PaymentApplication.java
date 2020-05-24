80fde737130ae10aa3dc5589852005ea95b17e70
Renan Azzolim <azzolimrenan2@gmail.com>
2020-05-24 16:26:01 -0300

    created dev and prod profiles

--- src/main/java/br/com/payment/PaymentApplication.java
+++ src/main/java/br/com/payment/PaymentApplication.java
@@ -1,35 +1,8 @@
 package br.com.payment;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import br.com.payment.domain.Categoria;
-import br.com.payment.domain.Cidade;
-import br.com.payment.domain.Cliente;
-import br.com.payment.domain.Endereco;
-import br.com.payment.domain.Estado;
-import br.com.payment.domain.ItemPedido;
-import br.com.payment.domain.Pagamento;
-import br.com.payment.domain.PagamentoComBoleto;
-import br.com.payment.domain.PagamentoComCartao;
-import br.com.payment.domain.Pedido;
-import br.com.payment.domain.Produto;
-import br.com.payment.domain.enums.EstadoPagamento;
-import br.com.payment.domain.enums.TipoCliente;
-import br.com.payment.repositories.CategoriaRepository;
-import br.com.payment.repositories.CidadeRepository;
-import br.com.payment.repositories.ClienteRepository;
-import br.com.payment.repositories.EnderecoRepository;
-import br.com.payment.repositories.EstadoRepository;
-import br.com.payment.repositories.ItemPedidoRepository;
-import br.com.payment.repositories.PagamentoRepository;
-import br.com.payment.repositories.PedidoRepository;
-import br.com.payment.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class PaymentApplication implements CommandLineRunner {
---
+++ src/main/java/br/com/payment/config/DevConfig.java
@@ -0,0 +1,40 @@
+package br.com.payment.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.payment.services.DBService;
+
+/**
+ * Configurações específicas para o profile dev
+ * 
+ * @author Renan
+ *
+ */
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean intantiateDatabase() throws ParseException {
+
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+}
