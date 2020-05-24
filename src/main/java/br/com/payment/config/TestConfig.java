624fb57ca3b406e9a10ddf3d0ae8daabb7dae213
Renan Azzolim <azzolimrenan2@gmail.com>
2020-05-24 14:56:40 -0300

    created profile test

---
+++ src/main/java/br/com/payment/config/TestConfig.java
@@ -0,0 +1,31 @@
+package br.com.payment.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.payment.services.DBService;
+
+/**
+ * Configurações específicas para o profile test
+ * 
+ * @author Renan
+ *
+ */
+@Configuration
+@Profile("test")
+public class TestConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean intantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+}
